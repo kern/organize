80aee4aaf968541e65a3f946283493b6ce40030a
Alexander Kern <alex@kernul.com>
2010-09-21 09:31:35 -0700

    Added prefix support.

--- lib/organize/project.rb
+++ lib/organize/project.rb
@@ -1,10 +1,18 @@
 module Organize
   class Project
     
+    attr_reader :prefix
     attr_accessor :name
     
-    def initialize(name)
+    DEFAULT_PREFIX = '~/Projects'
+    
+    def initialize(name, prefix = DEFAULT_PREFIX)
       @name = name
+      @prefix = prefix
+    end
+    
+    def path
+      File.join(prefix, name)
     end
   end
 end
--- spec/organize/project_spec.rb
+++ spec/organize/project_spec.rb
@@ -2,8 +2,31 @@ require 'spec_helper'
 
 describe Organize::Project do
   let(:project) { Organize::Project.new 'Foo' }
+  subject { project }
   
   it 'should have a name' do
-    project.name.should == 'Foo'
+    subject.name.should == 'Foo'
+  end
+  
+  context 'when created without a prefix' do
+    its(:prefix) { should == '~/Projects' }
+  end
+  
+  context 'when created with a prefix' do
+    subject { Organize::Project.new 'Foo', '~/Prefix' }
+    it 'should use that prefix instead of the default one' do
+      subject.prefix.should == '~/Prefix'
+    end
+  end
+  
+  describe '#path' do
+    before { project.track_methods :prefix, :name }
+    subject { project.path }
+    
+    it 'should combine the prefix and name' do
+      subject.should have_received :prefix
+      subject.should have_received :name
+      subject.should == '~/Projects/Foo'
+    end
   end
 end
