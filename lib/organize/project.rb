89c7d96d2f5f4754b149df9c94ffac4160edaa02
Alexander Kern <alex@kernul.com>
2010-09-21 12:59:06 -0700

    Added support for dropbox and archives.

--- lib/organize/project.rb
+++ lib/organize/project.rb
@@ -1,18 +1,40 @@
 module Organize
   class Project
     
-    attr_reader :prefix
+    attr_reader :prefix, :shared_prefix
     attr_accessor :name
     
-    DEFAULT_PREFIX = '~/Projects'
+    DEFAULTS = {
+      :prefix => '~/Projects',
+      :shared_prefix => '~/Dropbox'
+    }
     
-    def initialize(name, prefix = DEFAULT_PREFIX)
+    def initialize(name, options = {})
       @name = name
-      @prefix = prefix
+      
+      options = DEFAULTS.merge(options)
+      @prefix = options[:prefix]
+      @shared_prefix = options[:shared_prefix]
     end
     
     def path
       File.join(prefix, name)
+    end
+    
+    def archive_path
+      File.join(path, 'Archive')
+    end
+    
+    def shared_path
+      File.join(shared_prefix, name)
+    end
+    
+    def shared_link_path
+      File.join(path, 'Shared')
+    end
+    
+    def project_archive_path
+      File.join(prefix, 'Archive')
     end
   end
 end
--- spec/organize/project_spec.rb
+++ spec/organize/project_spec.rb
@@ -12,21 +12,93 @@ describe Organize::Project do
     its(:prefix) { should == '~/Projects' }
   end
   
+  context 'when created without a shared prefix' do
+    its(:shared_prefix) { should == '~/Dropbox' }
+  end
+  
   context 'when created with a prefix' do
-    subject { Organize::Project.new 'Foo', '~/Prefix' }
+    subject { Organize::Project.new 'Foo', :prefix => '~/Prefix' }
     it 'should use that prefix instead of the default one' do
       subject.prefix.should == '~/Prefix'
     end
   end
   
+  context 'when created with a shared prefix' do
+    subject { Organize::Project.new 'Foo', :shared_prefix => '~/Shared' }
+    it 'should use that prefix instead of the default one' do
+      subject.shared_prefix.should == '~/Shared'
+    end
+  end
+  
   describe '#path' do
-    before { project.track_methods :prefix, :name }
+    before do
+      project.track_methods :prefix, :name
+      subject # Run the subject immediately
+    end
+    
     subject { project.path }
     
     it 'should combine the prefix and name' do
-      subject.should have_received :prefix
-      subject.should have_received :name
+      project.should have_received(:prefix)
+      project.should have_received(:name)
       subject.should == '~/Projects/Foo'
+    end
+  end
+  
+  describe '#archive_path' do
+    before do
+      project.track_methods :path
+      subject # Run the subject immediately
+    end
+    
+    subject { project.archive_path }
+    
+    it 'should tack on Archive to the end of the path' do
+      project.should have_received(:path)
+      subject.should == '~/Projects/Foo/Archive'
+    end
+  end
+  
+  describe '#shared_path' do
+    before do
+      project.track_methods :shared_prefix, :name
+      subject # Run the subject immediately
+    end
+    
+    subject { project.shared_path }
+    
+    it 'should tack on the name to the end of the shared path' do
+      project.should have_received(:shared_prefix)
+      project.should have_received(:name)
+      subject.should == '~/Dropbox/Foo'
+    end
+  end
+  
+  describe '#shared_link_path' do
+    before do
+      project.track_methods :path
+      subject # Run the subject immediately
+    end
+    
+    subject { project.shared_link_path }
+    
+    it 'should tack on Shared to the end of the path' do
+      project.should have_received(:path)
+      subject.should == '~/Projects/Foo/Shared'
+    end
+  end
+  
+  describe '#project_archive_path' do
+    before do
+      project.track_methods :prefix
+      subject # Run the subject immediately
+    end
+    
+    subject { project.project_archive_path }
+    
+    it 'should tack on Archive to the end of the prefix' do
+      project.should have_received(:prefix)
+      subject.should == '~/Projects/Archive'
     end
   end
 end
