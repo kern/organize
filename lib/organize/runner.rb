cc9170472aba1c0aff76471471086a440b926913
Alexander Kern <alex@kernul.com>
2010-10-05 09:47:01 -0700

    Creates other folder and such.

--- lib/organize/runner.rb
+++ lib/organize/runner.rb
@@ -3,10 +3,18 @@ require 'optitron'
 module Organize
   class Runner < Optitron::CLI
     desc 'Install stuff'
-    def install(file, source = '.')
-      # TODO: Make this method create the prefix, shared prefix, the .organize
-      # file, the other folder (with all its symlinks), the Inbox -> Desktop
-      # folder, and project archive folder.
+    def install(prefix = '~/Projects', shared_prefix = '~/Dropbox', inbox = '~/Desktop')
+      FileUtils.mkdir_p(prefix)
+      FileUtils.mkdir_p(File.join(prefix, 'Archive'))
+      FileUtils.mkdir_p(shared_prefix)
+      FileUtils.mkdir_p(File.join(prefix, 'Other'))
+      FileUtils.ln_s(inbox, File.join(prefix, 'Inbox'))
+      
+      ['Documents', 'Movies', 'Music', 'Pictures',  'Public', 'Sites'].each do |folder|
+        FileUtils.ln_s("~/#{folder}", File.join(prefix, 'Other', folder))
+      end
+      
+      # TODO: Make this method create the .organize file.
     end
   end
 end
